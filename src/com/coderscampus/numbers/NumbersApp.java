6d254e8f87fe3aa173df06486cb31341746985ec
sayazian <sahar.ayazian@gmail.com>
2025-08-22 19:53:57 -0700

    fixed future.get() location that was stopping the threads to run in parallel.

--- src/com/coderscampus/numbers/NumbersApp.java
+++ src/com/coderscampus/numbers/NumbersApp.java
@@ -1,6 +1,7 @@
 package com.coderscampus.numbers;
 
 import com.coderscampus.numbers.service.NumberService;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
@@ -8,12 +9,7 @@ public class NumbersApp {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         NumberService numberService = new NumberService();
         AtomicIntegerArray globalExtremes = new AtomicIntegerArray(new int[]{0, 0});
-//        numberService.findMinAndMax(globalExtremes);
-//        The above line can be run to scan the numbers and extract Min and Max.
-//        I disabled it to save time.
-        globalExtremes.set(0, 0);
-        globalExtremes.set(1, 14);
+        numberService.findMinAndMax(globalExtremes);
         numberService.reportStats(globalExtremes);
     }
-
 }
--- src/com/coderscampus/numbers/service/NumberService.java
+++ src/com/coderscampus/numbers/service/NumberService.java
@@ -3,6 +3,7 @@ package com.coderscampus.numbers.service;
 import com.coderscampus.assignment.Assignment8;
 import com.coderscampus.numbers.domain.NumberTaskCount;
 import com.coderscampus.numbers.domain.NumberTaskMinMax;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,20 +11,21 @@ import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class NumberService {
-    public void findMinAndMax(AtomicIntegerArray globalExtremes) {
-        Future<Integer[]> future;
+    public void findMinAndMax(AtomicIntegerArray globalExtremes) throws ExecutionException, InterruptedException {
+        Assignment8 assignment = new Assignment8();
+        Future<Integer[]>[] futures = new Future[1000];
         Integer[] extremes;
-        try (ExecutorService service = Executors.newCachedThreadPool()) {
-            for (int i = 0; i < 1000; i++) {
-                future = service.submit(new NumberTaskMinMax(new Assignment8()));
-                extremes = future.get();
-                compareWithGlobals(extremes, globalExtremes);
-                System.out.println("Min : " + extremes[0] + " and Max : " + extremes[1]);
-            }
-        } catch (ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 1000; i++) {
+            futures[i] = executorService.submit(new NumberTaskMinMax(assignment));
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            extremes = futures[i].get();
+            compareWithGlobals(extremes, globalExtremes);
         }
         System.out.println("Global Min : " + globalExtremes.get(0) + " and Max : " + globalExtremes.get(1));
+        executorService.shutdown();
     }
 
     private static void compareWithGlobals(Integer[] extremes, AtomicIntegerArray globalExtremes) {
@@ -39,9 +41,13 @@ public class NumberService {
         Assignment8 assignment = new Assignment8();
         AtomicIntegerArray stats = new AtomicIntegerArray(new int[globalExtremes.get(1) - globalExtremes.get(0) + 1]);
         ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<Integer[]>[] futures = new Future[1000];
         for (int i = 0; i < 1000; i++) {
-            Future<Integer[]> future = executorService.submit(new NumberTaskCount(assignment, globalExtremes));
-            Integer[] taskStats = future.get();
+            futures[i] = executorService.submit(new NumberTaskCount(assignment, globalExtremes));
+
+        }
+        for (int i = 0; i < 1000; i++) {
+            Integer[] taskStats = futures[i].get();
             for (int j = 0; j < stats.length(); j++) {
                 stats.set(j, stats.addAndGet(j, taskStats[j]));
             }
@@ -49,5 +55,6 @@ public class NumberService {
         for (int i = 0; i < stats.length(); i++) {
             System.out.println(i + globalExtremes.get(0) + "= " + stats.get(i));
         }
+        executorService.shutdown();
     }
 }
